d37afcc5c05f38afec493af38f675dbc78effe51
DC004 <dc004@nick.local>
2015-12-30 10:55:28 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by DC004 on 15/12/30.
+//  Copyright © 2015年 DC004. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
